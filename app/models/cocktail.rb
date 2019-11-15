96dfe051c71028ad445ab81ae3fcc052dc5607c1
Bernard Le Moullec <bernard.lemoullec@gmail.com>
2019-11-15 15:31:15 +0100

    Added Cloudinary support

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,7 +3,7 @@
 # Missing top-level class documentation comment.
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
-  # When you delete a cocktail, you should delete associated doses (but not the ingredients as they can be linked to other cocktails).
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
+  has_one_attached :photo
 end
