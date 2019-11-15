89140855441dc5937dd7a89fd9027b64c7e0bd91
Bernard Le Moullec <bernard.lemoullec@gmail.com>
2019-11-15 12:40:59 +0100

    Update Gemfile & Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to:'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
